e9557a9bb7ea943695966b3a3ea1ea9c0d1cd4aa
shu <shu@deb>
2017-02-24 23:50:09 +0800

    首页做好了
    TODO 安全更新和debain新闻自动更新

--- src/js/ls_script.js
+++ src/js/ls_script.js
@@ -5,18 +5,26 @@
     } );
     /* 设置导航菜单 固定在页面最上方 */
     function topFixed(){
-	if( window.innerWidth >= 400 && window.scrollY >= 63 ){
+	if( window.innerWidth >= 700 && window.scrollY >= 63 ){
 	    $('.menuListDiv').css({
 		'position':'fixed',
 		'top':'0',
 		'left':'0'
 	    });
-	}else if( window.innerWidth >= 400 ){
+	}else if( window.innerWidth >= 700 ){
 	    $('.menuListDiv').css({
 		'position':'relative',
 		'top':'0',
 		'left':'0'
 	    });
+	    $( '.menuList' ).show();
+	}else if( window.innerWidth <= 700 ){
+	    $('.menuListDiv').css({
+		'position':'relative',
+		'top':'0',
+		'left':'0'
+	    });
+/*	    $( '.menuList' ).hide();*/
 	}
     }
     $( window ).bind( 'load', function(){
